2e10e6d9d267c24c37a5a830e387a21d5c31197a
Jose Bujalance <joseanbu23@gmail.com>
2021-09-12 18:09:13 +0200

    Add theme color

--- components/HeadTag.tsx
+++ components/HeadTag.tsx
@@ -9,6 +9,7 @@ export default function HeadTag(props: Props) {
     <Head>
       <title>{props.title}</title>
       <meta name="description" content="The personal website of José Bujalance, a full stack web developer" />
+      <meta name="theme-color" content="#485fc7" />
       <link rel="icon" href="/favicon.svg" />
     </Head>
   );
